5689c5a92c490145dce9c2b19d441bea5214bf79
stonerains <72440538+stonerains@users.noreply.github.com>
2020-11-03 09:51:54 +0900

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -133,8 +133,8 @@ class CarState(CarStateBase):
         ret.gearShifter = GearShifter.park
       elif gear == 7:
         ret.gearShifter = GearShifter.reverse
-#      else:
-#        ret.gearShifter = GearShifter.unknown
+      else:
+        ret.gearShifter = GearShifter.unknown
     # Gear Selecton - This is not compatible with all Kia/Hyundai's, But is the best way for those it is compatible with
     else:
       gear = cp.vl["LVR12"]["CF_Lvr_Gear"]
